5fc21c019ea2e6ec4a11e44f5d5b2dde5448597f
Alexandreil <36792981+Alexandreil@users.noreply.github.com>
2018-07-23 13:27:19 +0300

    Update trans.py

--- trans.py
+++ trans.py
@@ -4,15 +4,15 @@ def start_click():
 	#Удаляем ненужное
 	B.destroy()
 	canvas.destroy()
+	label2.destroy()
 	#Создаем вторую страницу
 	canvas1.pack()
 	#Размешаем поля
-	text1.place(x = 25, y = 50)
-	text2.place(x = 415, y = 50)
+	text1.place(x = 25, y = 70)
+	text2.place(x = 515, y = 70)
 	#Прокрутка
-	scroll_1.place(x = 390, y = 50)
-	scroll_2.place(x = 780, y = 50)
-
+	scroll_1.place(x = 390, y = 70, height = 150)
+	scroll_2.place(x = 880, y = 70, height = 150)
 	label1.place(x=10,y=10)
 #Переносим на другое поле
 def getText(event):
@@ -31,23 +31,22 @@ text = "Здравствуйте! Вы открыли\nпрограмму для
 label2 = Label(text=text, justify=CENTER, font = ("Times New Roman", "14"), bg = "#C2C2C2")
 label2.place(x = 10, y = 10)
 
-B = Button(root, text = "Пропустить", width = 9, height = 1, bg = "#C2C2C2", command = start_click)
+B = Button(root, text = "Пропустить", width = 9, height = 1,bg = "#C2C2C2", command = start_click)
 B.place(x=110, y=320)
 	
-canvas1 = Canvas(root, width = 800, height = 400, bg = "#FFFFFF")
-
+canvas1 = Canvas(root, width = 900, height = 400, bg = "#FFFFFF")
 #Поля ввода для перевода 
-text1=Text(root, width = 40, height = 8, font = ("Times New Roman", "14"), wrap = WORD)
-text2=Text(root, width = 40, height = 8, font = ("Times New Roman", "14"), wrap = WORD)
+text1=Text(canvas1, width = 40, height = 8, font = ("Times New Roman", "14"), wrap = WORD)
+text2=Text(canvas1, width = 40, height = 8, font = ("Times New Roman", "14"), wrap = WORD)
 
-scroll_1 = Scrollbar(command=text1.yview)
-scroll_2 = Scrollbar(command=text2.yview) 
+scroll_1 = Scrollbar(canvas1, orient="vertical", command=text1.yview)
+scroll_2 = Scrollbar(canvas1, orient="vertical", command=text2.yview) 
+
+text1.configure(yscrollcommand=scroll_1.set)
+text2.configure(yscrollcommand=scroll_2.set)
+
+label1 = Label(canvas1, text="NeighborПереводчик", fg="black",  font = ("Times New Roman", "20"), bg = "#FFFFFF")
 
 root.bind("<Key>", getText)
-
-text1.config(yscrollcommand=scroll_1.set)
-text2.config(yscrollcommand=scroll_2.set)
-
-label1 = Label(text="NeighborПереводчик", fg="black",  font = ("Times New Roman", "20"), bg = "#FFFFFF")
 
 root.mainloop()
